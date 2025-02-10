baec970e60661054724673ef225c4fc8d02217f2
Kodzito11 <Kos001@edu.zealand.dk>
2025-02-10 14:59:45 +0100

    Fikset Watch fejl (duplikate klasser)

--- WatchLibrary/Models/Watch.cs
+++ WatchLibrary/Models/Watch.cs
@@ -4,20 +4,78 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace WatchLibrary.Models
+namespace WatchLibrary
 {
 	public class Watch
 	{
 		public int Id { get; set; }
-		public string Brand { get; set; }
-		public string Model { get; set; }
-		public string ReferenceNumber { get; set; }
+		public string? Brand { get; set; }
+		public string? Model { get; set; }
+		public string? ReferenceNumber { get; set; }
 		public int Year { get; set; }
 		public string? Accessories { get; set; } //Alle ure kommer ikke nøvendigvis med tilbehør
 		public string? Functions { get; set; } //Alle ure har ikke nødvendigvis ekstra funktioner
 		public int Size { get; set; }
 		public int Condition { get; set; }
+		public string? Description { get; set; }
 
+		private void ValidateBrand()
+		{
+			if (string.IsNullOrWhiteSpace(Brand))
+			{
+				throw new ArgumentNullException(nameof(Brand), "Please insert a valid Brand");
+			}
+		}
+
+		private void ValidateModel()
+		{
+			if (string.IsNullOrWhiteSpace(Model))
+			{
+				throw new ArgumentNullException(nameof(Model), "Please insert a valid Model");
+			}
+		}
+
+		private void ValidateReferenceNumber()
+		{
+			if (string.IsNullOrWhiteSpace(ReferenceNumber))
+			{
+				throw new ArgumentNullException(nameof(ReferenceNumber), "Please insert a valid Reference Number");
+			}
+		}
+
+		private void ValidateYear()
+		{
+			if (Year < 1900 || Year > DateTime.Now.Year)
+			{
+				throw new ArgumentOutOfRangeException(nameof(Year), "Please insert a valid Year");
+			}
+		}
+
+		private void ValidateAccessories()
+		{
+			if (string.IsNullOrWhiteSpace(Accessories))
+			{
+				throw new ArgumentNullException(nameof(Accessories), "Please insert a valid Accessories");
+			}
+		}
+
+		private void ValidateFunctions()
+		{
+			if (string.IsNullOrWhiteSpace(Functions))
+			{
+				throw new ArgumentNullException(nameof(Functions), "Please insert a valid Functions");
+			}
+		}
+
+		public void Validate()
+		{
+			ValidateBrand();
+			ValidateModel();
+			ValidateReferenceNumber();
+			ValidateYear();
+			ValidateAccessories();
+			ValidateFunctions();
+		}
 
 
 
