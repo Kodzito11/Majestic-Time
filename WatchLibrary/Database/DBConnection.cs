aea81a16bafc076769461617aa3a02d7109ed3ae
Roman Ghazi <rog001@edu.zealand.dk>
2025-02-14 21:03:04 +0100

    DB Connection klasse og User repo opdateret i db format. Ikke hårdkodet liste.

---
+++ WatchLibrary/Database/DBConnection.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.Data.SqlClient;
+
+namespace WatchLibrary.Database
+{
+    public class DBConnection
+    {
+        private readonly string _connectionString;
+
+       //Pakker værdien ind: gemmer stringen
+        public DBConnection(string connectionString) 
+        {
+            _connectionString = connectionString;
+        }
+
+
+        
+        // ADO.NET-klasse, der opretter en ny forbindelse til SQL-databasen
+        public SqlConnection GetConnection()
+        {
+            return new SqlConnection(_connectionString);
+        }
+    }
+}
--- WatchLibrary/Repositories/UserRepository.cs
+++ WatchLibrary/Repositories/UserRepository.cs
@@ -1,69 +1,196 @@
-﻿using System;
+﻿using Microsoft.Data.SqlClient;
+using System;
 using System.Collections.Generic;
+using System.Data;
+using System.Data.Common;
 using System.Linq;
+using System.Reflection.Metadata;
 using System.Xml.Linq;
+using WatchLibrary.Database;
 using WatchLibrary.Models;
 
 namespace WatchLibrary.Repositories
 {
-	public class UserRepository
-	{
-		private int _nextId = 1; // Holder styr på næste unikke ID
-		private readonly List<User> _users = new(); // Liste over brugere
+    public class UserRepository
+    {
 
-		public UserRepository()
-		{
-			// Tilføjer nogle startbrugere ved oprettelse af repository
-			_users.Add(new User { Id = _nextId++, Username = "Roman", Email = "roman@example.com", Password = "hashedpassword1" });
-			_users.Add(new User { Id = _nextId++, Username = "Kodo", Email = "kodo@example.com", Password = "hashedpassword2" });
-			_users.Add(new User { Id = _nextId++, Username = "Joutiar", Email = "Joutiar@example.com", Password = "hashedpassword3" });
-		}
+        private readonly DBConnection _dbConnection;
 
-		public List<User> GetAll()
-		{
-			// Returnerer en kopi af listen for at beskytte interne data
-			return new List<User>(_users);
-		}
+        public UserRepository(DBConnection dbconnection)
+        {
+            _dbConnection = dbconnection;
+        }
 
-		public User? GetById(int id)
-		{
-			// Finder en bruger baseret på ID
-			return _users.Find(user => user.Id == id);
-		}
+        public List<User> GetAll()
+        {
+            var users = new List<User>();
+            var conn = _dbConnection.GetConnection();
+            var cmd = new SqlCommand("SELECT Id, Mail, Password FROM Users", conn);
 
-		public User Add(User user)
-		{
-			// Validerer bruger før tilføjelse
-			user.Validate();
-			user.Id = _nextId++; // Tildeler et unikt ID
-			_users.Add(user);
-			return user;
-		}
+            try
+            {
+                conn.Open(); //Starter forbindelsen, så vi kan snakke med databasen.
+                var reader = cmd.ExecuteReader(); //Udfører SQL'en og giver os adgang til de data, vi har bedt om.
+                while (reader.Read()) //gennemgår hver række
+                {
+                    var user = new User
+                    {
+                        Id = reader.GetInt32(0),
+                        Email = reader.GetString(1),
+                        Password = reader.GetString(2)
+                    };
+                    users.Add(user);
+                }
+                reader.Close();
+                return users;
+            }
+            catch (Exception ex)
+            {
+                throw new Exception("Fejl ved hentning af brugere", ex);
+            }
+            finally
+            {
+                if (conn.State == ConnectionState.Open)
+                {
+                    conn.Close();
+                }
+            }
+        }
 
-		public User? Remove(int id)
-		{
-			// Finder brugeren, der skal fjernes
-			User? user = GetById(id);
-			if (user == null)
-			{
-				return null; // Returnerer null, hvis brugeren ikke findes
-			}
-			_users.Remove(user);
-			return user;
-		}
 
-		public User? Update(int id, User user)
-		{
-			user.Validate();
-			User? existingUser = GetById(id);
-			if (existingUser == null)
-			{
-				return null;
-			}
-			existingUser.Username = user.Username;
-			existingUser.Email = user.Email;
-			existingUser.Password = user.Password;
-			return existingUser;
-		}
-	}
+
+
+        public User GetById(int id)
+        {
+            var conn = _dbConnection.GetConnection();
+            var cmd = new SqlCommand("SELECT Id, Mail, Password FROM Users WHERE Id = @Id", conn);
+            cmd.Parameters.AddWithValue("@Id", id);
+
+            try
+            {
+                conn.Open();
+                var reader = cmd.ExecuteReader();
+                if (reader.Read())
+                {
+                    var user = new User
+                    {
+                        Id = reader.GetInt32(0),
+                        Email = reader.GetString(1),
+                        Password = reader.GetString(2)
+                    };
+                    reader.Close();
+                    return user;
+                }
+                reader.Close();
+                return null;
+            }
+            catch (Exception ex)
+            {
+                throw new Exception("Fejl ved hentning af bruger", ex);
+            }
+            finally
+            {
+                if (conn.State == ConnectionState.Open)
+                {
+                    conn.Close();
+                }
+            }
+        }
+
+
+        public User Add(User user)
+        {
+
+
+            user.Validate();
+            var conn = _dbConnection.GetConnection();
+            var cmd = new SqlCommand("Insert into Users (username, mail, password, role) values (@username, @mail, @password, @role); SELECT SCOPE_IDENTITY()", conn);
+            cmd.Parameters.AddWithValue("@username", user.Username);
+
+
+            cmd.Parameters.AddWithValue("@mail", user.Email);
+            cmd.Parameters.AddWithValue("@password", user.Password);
+            cmd.Parameters.AddWithValue("@role", user.Role.ToString()); // Rolle er enum, så vi gemmer det som en streng
+
+            try
+            {
+                conn.Open();  // Åbner forbindelsen
+                var newId = Convert.ToInt32(cmd.ExecuteScalar());  // Kører kommandoen og får ID'et tilbage
+                user.Id = newId;  // Sætter det nye ID på brugerobjektet
+                return user;  // Returnerer den tilføjede bruger
+            }
+            catch (Exception ex)
+            {
+                throw new Exception("Fejl ved tilføjelse af bruger", ex);
+            }
+            finally
+            {
+                conn.Close();
+            }
+        }
+
+
+
+
+        public User? Remove(int id)
+        {
+            var conn = _dbConnection.GetConnection();
+            var cmd = new SqlCommand("DELETE FROM Users WHERE Id = @Id", conn);
+            cmd.Parameters.AddWithValue("@Id", id);
+
+            try
+            {
+                conn.Open();
+                int rowsAffected = cmd.ExecuteNonQuery();// ExecuteNonQuery() Når du ændrer data, men ikke skal have data tilbage.
+
+                if (rowsAffected > 0) //Lykkedes at slette
+                {
+                    return new User { Id = id };  // Retunerer id'et på slettet bruger
+                }
+                return null;  // Ingen rækker slettet
+            }
+            catch (Exception ex)
+            {
+                throw new Exception("Fejl ved sletning af bruger", ex);
+            }
+            finally
+            {
+                conn.Close();
+            }
+        }
+
+        public User? Update(User user)
+        {
+            user.Validate();  // Validerer brugerdata
+
+            var conn = _dbConnection.GetConnection();
+            var cmd = new SqlCommand("UPDATE Users SET username = @username, mail = @mail, password = @password, role = @role WHERE Id = @Id", conn);
+
+            cmd.Parameters.AddWithValue("@username", user.Username);
+            cmd.Parameters.AddWithValue("@mail", user.Email);
+            cmd.Parameters.AddWithValue("@password", user.Password);
+            cmd.Parameters.AddWithValue("@role", user.Role.ToString());
+            cmd.Parameters.AddWithValue("@Id", user.Id);
+
+            try
+            {
+                conn.Open();
+                int rowsAffected = cmd.ExecuteNonQuery();  // Kører opdateringskommandoen
+
+                if (rowsAffected > 0)
+                {
+                    return user;  // Returnerer den opdaterede bruger
+                }
+                return null;  // Returnerer null, hvis ingen bruger blev opdateret (fx hvis ID ikke findes)
+            }
+            catch (Exception ex)
+            {
+                throw new Exception("Fejl ved opdatering af bruger", ex);
+            }
+            finally
+            {
+                conn.Close();  // Lukker forbindelsen
+            }
+        }
+    }
 }
