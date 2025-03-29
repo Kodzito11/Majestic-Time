cc3c2d49d91111754744ad5927f47db907b4ded9
Kodzito11 <Kos001@edu.zealand.dk>
2025-03-29 23:37:05 +0100

    Har tilføjet 2 klasser i models
    OrderDTO, med kurv egenskaber
    
    OrderItemDTO, En kurv der kan have watch objekter i sig (watchID)
    
    Tilføjer repository (OrderRepository):
    
    - SaveOrder(OrderDTO order) indsætter i Orders og OrderItems tabeller
    
    - SQL bruger SqlCommand og transaktioner
    
    - Parametriseret SQL → modstår SQL Injection

---
+++ WatchLibrary/Models/OrderDTO.cs
@@ -0,0 +1,9 @@
+﻿namespace WatchLibrary.Models
+{
+	public class OrderDTO
+	{
+		public string CustomerName { get; set; }
+		public string Address { get; set; }
+		public List<OrderItemDTO> Items { get; set; }
+	}
+}
---
+++ WatchLibrary/Models/OrderItemsDTO.cs
@@ -0,0 +1,8 @@
+﻿namespace WatchLibrary.Models
+{
+	public class OrderItemDTO
+	{
+		public int WatchId { get; set; }
+		public int Quantity { get; set; }
+	}
+}
