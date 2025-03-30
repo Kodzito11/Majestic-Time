542022af90e3d446231d565609d7f6d13e4620dd
Roman Ghazi <rog001@edu.zealand.dk>
2025-03-30 19:55:49 +0200

    CartItem klasse

---
+++ WatchLibrary/Models/CartItem.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace WatchLibrary.Models
+{
+    public class CartItem
+    {
+        public int WatchId { get; set; }
+        public int Quantity { get; set; }
+        public decimal TotalPrice { get; set; }
+        public int UserId { get; set; }
+
+      
+        private void ValidateCartItem()
+        {
+            if (Quantity <= 0)
+            {
+                throw new ArgumentOutOfRangeException(nameof(Quantity), "You must add at least one item to your cart.");
+            }
+
+            
+            if (TotalPrice <= 0)
+            {
+                throw new ArgumentException("Total Price must be greater than 0 and cannot be negative.", nameof(TotalPrice));
+            }
+        }
+
+       
+        public void Validate()
+        {
+            ValidateCartItem();
+
+           
+            if (WatchId <= 0)
+            {
+                throw new ArgumentException("Invalid WatchId.", nameof(WatchId));
+            }
+
+            
+            if (Quantity <= 0)
+            {
+                throw new ArgumentOutOfRangeException(nameof(Quantity), "Quantity must be greater than zero.");
+            }
+
+            
+            if (UserId <= 0)
+            {
+                throw new ArgumentException("Invalid UserId.", nameof(UserId));
+            }
+        }
+
+        
+        public void CalculateTotalPrice(decimal pricePerItem)
+        {
+            if (pricePerItem <= 0)
+            {
+                throw new ArgumentException("Price per item must be greater than zero.", nameof(pricePerItem));
+            }
+
+            TotalPrice = pricePerItem * Quantity;
+        }
+
+    
+        public override string ToString()
+        {
+            return $"WatchId: {WatchId}, Quantity: {Quantity}, TotalPrice: {TotalPrice:C}, UserId: {UserId}";
+        }
+    }
+}
